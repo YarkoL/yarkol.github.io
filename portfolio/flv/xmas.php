5207b6f158cbc5f3b86e4439925c9bf433892ab3
Jarkko Lehto <lehto.jarkko@gmail.com>
2017-05-18 17:09:25 +0300

    Nuke everything.

--- portfolio/flv/xmas.php
+++
@@ -1,9 +0,0 @@
-<?php
-$title = "Android Christmas";
-$flv = "xmas.flv";
-$width = "640";
-$height = "340";
-$txt = "Animation to promote a webpage. Done with Anime Studio Pro 8 ";
-
-include("../page.php");
-?>
